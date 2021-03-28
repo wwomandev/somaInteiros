c2d970267ee57d5a17f1926f72c1bae72dd423d1
Carla Mariana A. Braga <80007433+wwomandev@users.noreply.github.com>
2021-03-27 22:46:34 -0300

    Add files via upload

---
+++ Soma Dois Numeros Inteiros.cpp
@@ -0,0 +1,21 @@
+// somaNumeros
+
+#include<stdio.h>
+int main(){
+	int A, B, SOMA;
+	printf("Digite a letra A: \n");
+	scanf("%d", &A);
+	printf("Digite a letra B: ");
+	scanf("%d", &B);
+	SOMA = A + B;
+	printf("%d + %d = %d", A, B, SOMA);
+	
+	
+	
+	
+	return 0;
+	
+	
+	
+	
+}
